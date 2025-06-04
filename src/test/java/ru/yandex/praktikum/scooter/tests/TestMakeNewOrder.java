21ede4eef75fb1786afd9fa97f11ed817e5d23b0
Vadim Evstegneev <ev.vadim@gmail.com>
2025-06-04 21:46:10 +0300

    Parameterized testing: implement 3 parameters

--- src/test/java/ru/yandex/praktikum/scooter/tests/TestMakeNewOrder.java
+++ src/test/java/ru/yandex/praktikum/scooter/tests/TestMakeNewOrder.java
@@ -3,6 +3,8 @@ package ru.yandex.praktikum.scooter.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,21 +12,38 @@ import ru.yandex.praktikum.scooter.pages.order.OrderPage;
 
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Parameterized.class)
 public class TestMakeNewOrder {
 
+    // готовим переменные для параметризации
+    private final String setBrowser;
+    private final String firstName;
+    private final String lastName;
+
+
     private WebDriver driver;
-    private String setBrowser = "ff";
+
+    public TestMakeNewOrder(String setBrowser, String firstName, String lastName) {
+        this.setBrowser = setBrowser;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getOrderData() {
+        return new Object[][] {
+                {"ff", "Василий", "Самокатов"},
+                {"chr", "Самокат", "Васильев"},
+        };
+    }
 
     private WebDriver prepareBrower() {
 
         switch (setBrowser) {
-
             case "ff":
                 return new FirefoxDriver();
-
             case "chr":
                 return new ChromeDriver();
-
             default:
                 return null;
         }
@@ -43,6 +62,7 @@ public class TestMakeNewOrder {
             case "chr":
                 WebDriverManager.chromedriver().setup();
                 break;
+
         }
     }
 
@@ -54,7 +74,7 @@ public class TestMakeNewOrder {
 
         OrderPage orderPage = new OrderPage(driver);
 
-        orderPage.makeNewOrder("Гарри", "Поттер", "Наклонная наб., 17", "арба", "79876543210", "24.09.2024", 7, "чёрный", "привезите заряженный самокат");
+        orderPage.makeNewOrder(firstName, lastName, "Наклонная наб., 17", "арба", "79876543210", "24.09.2024", 7, "чёрный", "привезите заряженный самокат");
 
         assertTrue("Неуспешное оформление заказа, текст «Заказ оформлен» не найден",orderPage.orderConfirmedTextDisplayed());
 
