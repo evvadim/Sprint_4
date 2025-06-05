7c8b9830e66a2f32ddc80f1b80ff8f526b03f2e7
Vadim Evstegneev <ev.vadim@gmail.com>
2025-06-05 23:34:11 +0300

    parameterized test accordion section done

--- src/test/java/ru/yandex/praktikum/scooter/tests/TestClickOnAccordionShowAccordionPanels.java
+++ src/test/java/ru/yandex/praktikum/scooter/tests/TestClickOnAccordionShowAccordionPanels.java
@@ -1,8 +1,11 @@
 package ru.yandex.praktikum.scooter.tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,11 +13,38 @@ import ru.yandex.praktikum.scooter.pages.main.SectionImportantQuestions;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class TestClickOnAccordionShowAccordionPanels {
 
     private WebDriver driver;
-    private String setBrowser = "ff";
 
+    // готовим переменные для параметризации
+    private final String setBrowser;
+    private final int index;
+    private final String message;
+
+    public TestClickOnAccordionShowAccordionPanels(String setBrowser,
+                                                   int index,
+                                                   String message) {
+        this.setBrowser = setBrowser;
+        this.index = index;
+        this.message = message;
+    }
+
+    // данные для тестирования
+    @Parameterized.Parameters
+    public static Object[][] getMessages() {
+        return new Object[][] {
+                {"chr", 1, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {"chr", 2, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {"chr", 3, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {"chr", 4, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {"chr", 5, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {"chr", 6, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {"chr", 7, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {"chr", 8, "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+        };
+    }
 
     private WebDriver prepareBrower() {
 
@@ -52,19 +82,16 @@ public class TestClickOnAccordionShowAccordionPanels {
 
         SectionImportantQuestions sectionImportantQuestions = new SectionImportantQuestions(driver);
 
-        int number = 5;
-        String text = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+        sectionImportantQuestions.clickAccordionItemNumber(index);
 
-        sectionImportantQuestions.clickAccordionItemNumber(number);
-        assertTrue(String.format("Панель эелемента номер %d не отображается", number), sectionImportantQuestions.accordionItemNumberIsDisplayed(number));
-        assertEquals(String.format("Текст элемента списка номер %d отличается от требований", number),
-                text, sectionImportantQuestions.getTextAccordionItemNumber(number));
-//        System.out.printf("текст элемента %d: %s", number, sectionImportantQuestions.getTextAccordionItemNumber(number));
+        assertTrue(String.format("Панель эелемента номер %d не отображается", index), sectionImportantQuestions.accordionItemNumberIsDisplayed(index));
+        assertEquals(String.format("Текст элемента списка номер %d отличается от требований", index),
+                message, sectionImportantQuestions.getTextAccordionItemNumber(index));
     }
 
-//    @After
-//    public void tearDown() {
-//        driver.quit();
-//    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
